44f7f3f4ca90e832cd82503e24ee6729cd7e64f1
Dalton Rowe <cdaltonrowe@gmail.com>
2025-03-10 12:50:49 +0100

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const nothing = 0.03; // some small spacing to prevent z-fighting
+export const tolerance = 0.2;
+
+export const hookRadius = 15;
+export const hookWidth = 5;
+export const hookThickness = 5;
+
+export const postLength = 30;
+
+export const bodySpacing = 50;
+
+export const bodyHeight = 30;
+export const bodyThickness = 6.5;
+export const bodyLength = 140;
+
+export const holeRadius = 1.6;
+export const holeHeight = bodyThickness + nothing;
